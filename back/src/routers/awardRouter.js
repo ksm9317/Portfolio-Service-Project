2f4ebc0b763e2f62721a6d2e4c6a39a7284b93d3
nowgnas <marshmello.sw@gmail.com>
2022-03-16 22:21:27 +0900

    [award] Add headers in awardRouter.js
    응답으로 토큰 전송 실패.

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -6,10 +6,11 @@ import { awardService } from "../services/awardService";
 
 const awardRouter = Router();
 
-awardRouter.post("/user/login", login_required, async (req, res, next) => {
+awardRouter.post("/user/login", async (req, res, next) => {
   // 로그인 토큰 발급용
   //   토큰 발급은 JWT_SECRET_KEY를 사용해서 발급하고 헤더에 authorization에 저장한다
   //    토큰 발급은 bearer + token으로 한다
+  console.log("award user login");
   try {
     const email = req.body.email;
     const password = req.body.password;
@@ -21,11 +22,16 @@ awardRouter.post("/user/login", login_required, async (req, res, next) => {
       },
       process.env.JWT_SECRET_KEY
     );
-    req.session.jwt = `bearer ${token}`;
+    // req.headers.authorization = `bearer ${token}`;
+    const saveToken = `bearer ${token}`;
 
-    return res.status.json({
-      succ: "ture",
-      token,
+    // TODO: 토큰 생성 확인하기
+    // fixme: 헤더에 토큰 저장을 어떻게 해야
+
+    return res.status(200).json({
+      headers: {
+        Athorization: saveToken,
+      },
     });
   } catch (error) {
     next(error);
@@ -43,15 +49,13 @@ awardRouter.post("/award/create", login_required, async (req, res, next) => {
     }
 
     // req에서 수상 내역으로 저장할 데이터 받아오기
-    const awardTitle = req.body.awardTitle;
-    const rank = req.body.rank;
-    const content = req.body.content;
+    const title = req.body.title;
+    const description = req.body.description;
 
     // 데이터를 award db에 추가하기
     const newAward = await awardService.addAward({
-      awardTitle,
-      rank,
-      content,
+      title,
+      description,
     });
 
     if (newAward.errorMessage) {
--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -5,6 +5,8 @@ class awardService {
   // 사용자의 수상 내역을 받아 model로 전송
   static async addAward({ title, description }) {
     const id = uuidv4();
+    const newAward = { id: id, title, description };
+
     const createdNewAward = await Award.create({ newAward });
     createdNewAward.errorMessage = null;
     return createdNewAward;
