0b03984eb6a35d6acd64fc798d9c3827112c8762
JIC <qlshdh@gmail.com>
2022-03-20 12:09:18 +0900

    [education] fix value output

--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,36 +1,38 @@
 import { Card, Button } from "react-bootstrap";
 
-function ProjectCard({ user, setIsEditing, isEditable }) {
-  const title = user?.title;
-  const description = user?.description;
-  const from_data = user?.from_data;
-  const to_data = user?.to_data;
+function ProjectCard({ project, setIsEditing, isEditable, isEditing }) {
+  const title = project?.title;
+  const description = project?.description;
+  const from_data = project?.from_data;
+  const to_data = project?.to_data;
 
   return (
     <div className="mb-2 ms-3 mr-5">
       <div>
-        <Card.Text>
-          <div className="justify-content-between align-items-center mb-2 row">
-            <div className="col">
-              {title} <br />
-              <span className="text-muted">{description}</span> <br />
-              <span className="text-muted">
-                {from_data}~{to_data}
-              </span>
+        {isEditing && (
+          <Card.Text>
+            <div className="justify-content-between align-items-center mb-2 row">
+              <div className="col">
+                {title} <br />
+                <span className="text-muted">{description}</span> <br />
+                <span className="text-muted">
+                  {from_data}~{to_data}
+                </span>
+              </div>
+              <div className="col - lg - 1 col">
+                {isEditable && (
+                  <Button
+                    variant="outline-info"
+                    size="sm"
+                    onClick={() => setIsEditing(true)}
+                  >
+                    편집
+                  </Button>
+                )}
+              </div>
             </div>
-            <div className="col - lg - 1 col">
-              {isEditable && (
-                <Button
-                  variant="outline-info"
-                  size="sm"
-                  onClick={() => setIsEditing(true)}
-                >
-                  편집
-                </Button>
-              )}
-            </div>
-          </div>
-        </Card.Text>
+          </Card.Text>
+        )}
       </div>
     </div>
   );
